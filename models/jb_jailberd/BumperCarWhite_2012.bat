8b977f0eca4cf88d82db0443778ec3ef2c50483b
retrontology <retrontology@hotmail.com>
2018-07-22 11:49:28 -0500

    first commit

---
+++ models/jb_jailberd/BumperCarWhite_2012.bat
@@ -0,0 +1,4 @@
+@echo on
+REM Compiling using the Wall Worm Batch File...
+"C:\Program Files (x86)\Steam\SteamApps\common\Counter-Strike Global Offensive\bin\studiomdl.exe" -game "C:\Program Files (x86)\Steam\SteamApps\common\Counter-Strike Global Offensive\csgo" -nop4 "C:\Program Files (x86)\Steam\SteamApps\common\Counter-Strike Global Offensive\csgo\models\jb_jailberd\BumperCarWhite.qc"
+@pause
